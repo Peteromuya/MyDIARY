b7c8434ddf11bcb9201efb97b172062068cc006f
Peteromuya <pomuya89@example.com>
2018-07-24 00:06:16 +0300

    Add models

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-"""Handles data storage for Users, Diaries and Entries
+"""Handles data storage for Users and Diaries
 """
 
 all_users = {}
@@ -6,11 +6,6 @@ user_count = 1
 
 all_diaries = {}
 diary_count = 1
-
-all_entries = {}
-entry_count = 1
-
-
 
 
 class User(object):
@@ -79,35 +74,6 @@ class Diary(object):
             return {"message" : "diary number does not exist"}
 
 
-class Entries(object):
-    """Contains methods to add, update and delete a entry options"""
 
-
-    @staticmethod
-    def create_entry(entry_option, todo, **kwargs):
-        """Creates a new entry option and appends this information to the all_entries dictionary"""
-        global all_entries
-        global entry_count
-        all_entries[entry_count] = {"id": entry_count, "entry_option" : entry_option, "to-do": todo}
-        new_entry_option = all_entries[entry_count]
-        entry_count += 1
-        return new_entry_option
-
-    @staticmethod
-    def update_entries(entry_id, entry_option, todo, **kwargs):
-        """Updates entry option information in entries"""
-        if entry_id in all_entries.keys():
-            all_entries[entry_id] = {"id": entry_id, "entry_option" : entry_option, "to-do" : to-do}
-            return all_entries[entry_id]
-        return {"message" : "entry option does not exist"}
-
-    @staticmethod
-    def delete_entry(entry_id):
-        """Deletes an entry_option from the entries"""
-        try:
-            del all_entries[entry_id]
-            return {"message" : "entry option successfully deleted"}
-        except KeyError:
-            return {"message" : "entry option does not exist"}
 
 
